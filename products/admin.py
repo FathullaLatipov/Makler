f17d188c3856b322aaccd941ebe9e658d6655100
User <user@MacBook-Air-User.local>
2022-09-19 00:44:06 +0500

    change settings

--- products/admin.py
+++ products/admin.py
@@ -15,3 +15,4 @@ class HouseModelAdmin(admin.ModelAdmin):
     list_display = ['title', 'price', 'category', 'type', 'created_at']
     search_fields = ['title', 'type']
     list_filter = ['created_at']
+
