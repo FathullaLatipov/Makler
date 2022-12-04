d9009f28d2cd97eee7064572c2fefcd816c45e11
User <user@MacBook-Air-User.local>
2022-12-04 20:45:47 +0500

    delete image in master

--- products/views.py
+++ products/views.py
@@ -40,7 +40,7 @@ class AmenitiesListAPIView(generics.ListAPIView):
 
 
 class StandardResultsSetPagination(PageNumberPagination):
-    page_size = 5
+    page_size = 8
     page_size_query_param = 'page_size'
     max_page_size = 1000
 
