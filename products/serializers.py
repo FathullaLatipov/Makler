51527eeeca530764c3525cfc0c88d9b22a74e1f4
User <user@MacBook-Air-User.local>
2022-12-06 13:02:52 +0500

    new pagination and price_type2

--- products/serializers.py
+++ products/serializers.py
@@ -244,7 +244,7 @@ class HomeSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = HouseModel
-        fields = ['title', 'descriptions', 'price', 'phone_number', 'app_currency', 'app_type', 'typeOfRent',
+        fields = ['id', 'title', 'descriptions', 'price', 'phone_number', 'app_currency', 'app_type', 'typeOfRent',
                   'typeOfHouse',
                   'typeOfObject', 'app_ipoteka', 'app_mebel', 'type', 'address', 'general', 'residential',
                   'number_of_rooms', 'floor', 'floor_from', 'building_type', 'amenities', 'product_status',
