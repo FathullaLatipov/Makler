bb6b9eacbdd6c719b5a7e46611c88349fdd6f7ee
FathullaLatipov <fathullaxonlatipov@gmail.com>
2023-01-07 20:23:12 +0500

    new timedata2

--- makler/settings.py
+++ makler/settings.py
@@ -180,7 +180,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(weeks=60),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=60),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
 
